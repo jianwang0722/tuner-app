0af9f51308a7987ce4c45ef4216b71c25db07fb9
jianwang0722 <jianw.wang@mail.utoronto.ca>
2023-07-20 13:27:31 +0800

    Initial Commit

---
+++ tuner/tunerApp.swift
@@ -0,0 +1,17 @@
+//
+//  tunerApp.swift
+//  tuner
+//
+//  Created by Jian Wang on 2023-07-20.
+//
+
+import SwiftUI
+
+@main
+struct tunerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
