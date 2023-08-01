e59bc27254d04c4e50d4a7ae7e57142e984ded35
jianwang0722 <jianw.wang@mail.utoronto.ca>
2023-08-02 00:48:12 +0800

    initial UI design

--- tuner/ContentView.swift
+++ tuner/ContentView.swift
@@ -8,19 +8,96 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var isTunerMode = true
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        if (isTunerMode) {
+            TunerModeView()
+        } else {
+            InstrumentModeView()
         }
-        .padding()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct TunerModeView: View {
+    var body: some View {
+        ZStack {
+            Color("darkGray")
+                .edgesIgnoringSafeArea(.all)
+            
+            // Buttons at the bottom of the screen
+            VStack{
+                Spacer()
+                // Placeholder Pitch value to be implemented
+                Text("A")
+                    .font(.system(size: 100, weight: .bold))
+                    .foregroundColor(.white)
+                // Placeholder Frequency value to be implemented
+                Text("440Hz")
+                    .font(.system(size: 40, weight: .semibold))
+                    .foregroundColor(.gray)
+                Spacer()
+                HStack {
+                    TunerButton()
+                    InstrumentButton()
+                }
+            }
+        }
+    }
+}
+
+struct InstrumentModeView: View {
+    var body: some View {
+        ZStack {
+            Color("darkGray")
+                .edgesIgnoringSafeArea(.all)
+            
+            VStack{
+                
+                Spacer()
+                HStack {
+                    TunerButton()
+                    InstrumentButton()
+                }
+            }
+        }
+    }
+}
+
+struct TunerButton: View {
+    var body: some View {
+        Button {
+            print("tapped")
+        } label: {
+            Image(systemName: "tuningfork")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .bold()
+                .frame(width: 150, height: 30)
+                
+        }
+        .padding()
+    }
+}
+
+struct InstrumentButton: View {
+    var body: some View {
+        Button {
+            print("tapped")
+        } label: {
+            Image(systemName: "music.note")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .bold()
+                .frame(width: 150, height: 30)
+        }
+        .padding()
     }
 }
